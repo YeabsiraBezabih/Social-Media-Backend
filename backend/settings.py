1215ac5300da260858592fffd64499847179db44
yeabsira <yeabsirabezabih791@gmail.com>
2025-04-14 09:45:22 +0300

    Setup Swagger API documentation with drf-yasg

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', # Django REST Framework
+    'rest_framework_simplejwt', # JWT Authentication
+    'api',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
